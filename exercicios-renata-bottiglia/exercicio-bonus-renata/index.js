ee04c1e078538c62efa8cabbba870bd27fbd81f1
rbottiglia <renatabottiglia@gmail.com>
2019-11-29 00:47:13 -0300

    exercicio - renata bottiglia

---
+++ exercicios-renata-bottiglia/exercicio-bonus-renata/index.js
@@ -0,0 +1,23 @@
+function carregar(){
+
+    let msg = document.querySelector('#msg');
+    let img = document.querySelector('#fotoDoDia');
+    let data = new Date();
+    // let hora = data.getHours();
+
+    let hora = 19
+
+    msg.innerText = `Agora são ${hora} horas.`
+
+    if(hora >= 0 && hora < 12){
+        img.src = './img/bom-dia.jpg';
+        document.body.style.backgroundColor = '#bb9679';
+    } else if (hora >= 12 && hora < 18){
+    	img.src = '.img/boa-tarde.jpg';
+    	document.body.style.backgroundColor = "blue";
+    } else {
+    		img.src = './img/boa-noite.jpg';
+    	    document.body.style.backgroundColor = "yellow";
+
+        
+    }    }
