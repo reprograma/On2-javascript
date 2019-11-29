f108a12faf0bc57621702b62319a1685f9f70cfc
Stela Bruno <stelabruno0@gmail.com>
2019-11-29 19:50:03 -0300

    exercicios casa e bônus da aula de Javascript

---
+++ exercicios-stela-bruno/exercicio-casa-stela-bruno/index.js
@@ -0,0 +1,60 @@
+let form = document.getElementById('formulario');
+let entrada = document.getElementById('entrada');
+let tarefas = document.getElementById('tarefas');
+let selecionar = document.getElementById('selecionar');
+let excluir = document.getElementById('excluir');
+
+form.addEventListener('submit', function (evento) {
+    evento.preventDefault()
+
+    vazioEntrada = entrada.Value.trim()
+
+    if(vazioEntrada == ''){
+        alert('Digite uma tarefa válida')
+    } else { }
+        
+    let diVazia = document.createElement('div');
+    let lista = document.createElement('p');
+    let deletar = document.createElement('span');
+
+    diVazia.className = 'divTarefas';
+    lista.innerHTML = (entrada.value);
+    deletar.innerText = 'x';
+
+    deletar.style.cursor = 'pointer';
+
+    lista.classList = 'tarefas';
+    
+    tarefas.appendChild(diVazia);
+    diVazia.appendChild(lista);
+    diVazia.appendChild(deletar);
+
+    lista.addEventListener('click', function (p) {
+        if (lista.className == 'checado') {
+            lista.classList.remove('checado')
+        } else {
+            lista.className = 'checado'
+        }
+    })
+
+    deletar.addEventListener('click', function (span) {
+        diVazia.parentNode.removeChild(diVazia)
+    })
+    
+    entrada.value = ""
+
+    selecionar.addEventListener('click', function () {
+        if (!lista.classList.contains('checado')) {
+            lista.className = 'checado'
+            selecionar.innerHTML = 'Desmarcar Tarefas'
+        } else {
+            lista.classList.remove('checado')
+            selecionar.innerHTML = 'Selecionar Tarefas'
+        }
+
+    })
+
+    excluir.addEventListener('click', function () {
+        lista.parentNode.remove(lista)
+    })
+})
