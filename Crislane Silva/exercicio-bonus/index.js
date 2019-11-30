a80b34e77afd756ce84448e46498c64b0721f067
Crislane Silva <crislane.silva@gmail.com>
2019-11-29 23:34:06 -0300

    Exercicio bonus - Crislane Silva

--- Crislane Silva/exercicio-bonus/index.js
+++ Crislane Silva/exercicio-bonus/index.js
@@ -3,20 +3,20 @@ function carregar(){
     let msg = document.querySelector('#msg');
     let img = document.querySelector('#fotoDoDia');
     let data = new Date();
-    //let hora = data.getHours();
+    let hora = data.getHours();
 
-    let hora = 8
+    //let hora = 11
 
     msg.innerHTML = `Agora são ${hora} horas.`
 
     if(hora >= 0 && hora < 12){
         img.src = './img/bom-dia.jpg';
-        document.body.style.backgroundColor = '#bb9679';
-    } else if (hora >= 12 && hora < 18){
+        document.body.style.backgroundColor = '#EBC79E';
+    }else if (hora >= 12 && hora < 18){
         img.src = './img/boa-tarde.jpg';
-        document.body.style.backgroundColor = '#3a8da4';
-    } else {
+        document.body.style.backgroundColor = '#3299CC';
+    }else {
         img.src = './img/boa-noite.jpg';
-        document.body.style.backgroundColor = '#000f1f';
+        document.body.style.backgroundColor = '#000000'
     }
 }
