179ce7bf5cc58ad673a97a1e16d24361a9c913de
Carolinne Hosana <carolhosana@hotmail.com>
2019-12-01 21:49:54 -0300

    exercicio bonus js

--- exercicio-bonus/CarolHosana/index.js
+++ exercicio-bonus/CarolHosana/index.js
@@ -3,20 +3,20 @@ function carregar(){
     let msg = document.querySelector('#msg');
     let img = document.querySelector('#fotoDoDia');
     let data = new Date();
-    //let hora = data.getHours();
+    let hora = data.getHours();
 
-    let hora = 22    
+    //let hora = 15
 
-    msg.innerHTML = `Agora são ${hora} horas `
+    msg.innerHTML = `Agora são ${hora} horas.`
 
     if(hora >= 0 && hora < 12){
         img.src = './img/bom-dia.jpg';
-        document.body.style.backgroundColor = '#CDAA7D';
+        document.body.style.backgroundColor = '#FF4500';
     } else if (hora >= 12 && hora < 18){
         img.src = './img/boa-tarde.jpg';
-        document.body.style.backgroundColor = '#EE7600';
+        document.body.style.backgroundColor = '#DC143C';
     } else {
         img.src = './img/boa-noite.jpg';
-        document.body.style.backgroundColor = '#2F4F4F';
+        document.body.style.backgroundColor = '#A0522D';
     }
 }
